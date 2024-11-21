6d4ea2d52f7d55163a4505dd52a8dea26ba52d6c
Javier Lete <javier@eskura21.com>
2024-11-21 10:54:11 +0100

    Ejemplo de uso de Lambdas

--- bases/src/com/ipartek/bibliotecas/Consola.java
+++ bases/src/com/ipartek/bibliotecas/Consola.java
@@ -1,8 +1,11 @@
 package com.ipartek.bibliotecas;
 
+import java.time.LocalDate;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class Consola {
+	private static final boolean NO_REQUERIDO = false;
 	private static final Scanner SC = new Scanner(System.in);
 
 	private Consola() {
@@ -20,13 +23,39 @@ public class Consola {
 		System.out.println(mensaje);
 	}
 
-	public static String leerString(String mensaje) {
+	public static String leerString(String mensaje, boolean requerido) {
+		if (requerido) {
+			return leerAlgo(mensaje, texto -> {
+				if (texto.isBlank()) {
+					throw new IllegalArgumentException("El texto no puede estar vacío");
+				}
+
+				return texto.trim();
+			});
+		}
+
 		p(mensaje + ": ");
 		return SC.nextLine();
 	}
 
+	public static String leerString(String mensaje) {
+		return leerString(mensaje, NO_REQUERIDO);
+	}
+
 	public static int leerEntero(String mensaje) {
-		int valor = 0;
+		return leerAlgo(mensaje, Integer::parseInt);
+	}
+
+	public static LocalDate leerFecha(String mensaje) {
+		return leerAlgo(mensaje, LocalDate::parse);
+	}
+
+	public static Long leerLong(String mensaje) {
+		return leerAlgo(mensaje, Long::parseLong);
+	}
+
+	private static <T> T leerAlgo(String mensaje, Function<String, T> cachoCodigo) {
+		T valor = null;
 
 		boolean hayError = true;
 
@@ -34,13 +63,13 @@ public class Consola {
 			String texto = leerString(mensaje);
 
 			try {
-				valor = Integer.parseInt(texto);
+				valor = cachoCodigo.apply(texto);
 				hayError = false;
-			}catch (NumberFormatException e) {
+			} catch (Exception e) {
 				pl("El valor introducido no es válido");
 			}
 		} while (hayError);
-		
+
 		return valor;
 	}
 }
