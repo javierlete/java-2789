08f39e53f98a47f8d3189283044445d68bf160c9
Javier Lete <javier@eskura21.com>
2024-11-25 10:10:13 +0100

    Gestión de almacén: pojos

---
+++ bases/src/com/ipartek/almacen/pojos/Producto.java
@@ -0,0 +1,77 @@
+package com.ipartek.almacen.pojos;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Producto {
+	private Long id;
+	private String nombre;
+	private BigDecimal precio;
+	private LocalDate fechaCaducidad;
+
+	public Producto(Long id, String nombre, BigDecimal precio, LocalDate fechaCaducidad) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.fechaCaducidad = fechaCaducidad;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public BigDecimal getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(BigDecimal precio) {
+		this.precio = precio;
+	}
+
+	public LocalDate getFechaCaducidad() {
+		return fechaCaducidad;
+	}
+
+	public void setFechaCaducidad(LocalDate fechaCaducidad) {
+		this.fechaCaducidad = fechaCaducidad;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(fechaCaducidad, id, nombre, precio);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Producto other = (Producto) obj;
+		return Objects.equals(fechaCaducidad, other.fechaCaducidad) && Objects.equals(id, other.id)
+				&& Objects.equals(nombre, other.nombre) && Objects.equals(precio, other.precio);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Producto [id=%s, nombre=%s, precio=%s, fechaCaducidad=%s]", id, nombre, precio,
+				fechaCaducidad);
+	}
+
+}
