4b347d58cf80d80301bea12662b9c72f05bec184
Javier Lete <javier@eskura21.com>
2024-11-28 10:15:21 +0100

    Modificamos la fábrica para devolver daos de Categoria

--- bases/src/com/ipartek/almacen/fabrica/Fabrica.java
+++ bases/src/com/ipartek/almacen/fabrica/Fabrica.java
@@ -4,22 +4,27 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Properties;
 
+import com.ipartek.almacen.accesodatos.DaoCategoria;
 import com.ipartek.almacen.accesodatos.DaoProducto;
 
 public class Fabrica {
 	private static DaoProducto daoProducto = null;
+	private static DaoCategoria daoCategoria = null;
 	
 	static {
 		try {
 			Properties props = new Properties();
 			props.load(Fabrica.class.getClassLoader().getResourceAsStream("almacen.properties"));
 			
-			var dao = props.getProperty("dao.implementacion");
+			var daoProductoClase = props.getProperty("dao.producto.implementacion");
+			var daoCategoriaClase = props.getProperty("dao.categoria.implementacion");
+			
 			var url = props.getProperty("dao.url");
 			var user = props.getProperty("dao.user");
 			var pass = props.getProperty("dao.pass");
 			
-			daoProducto = (DaoProducto) Class.forName(dao).getConstructor(String.class, String.class, String.class).newInstance(url, user, pass);
+			daoProducto = (DaoProducto) Class.forName(daoProductoClase).getConstructor(String.class, String.class, String.class).newInstance(url, user, pass);
+			daoCategoria = (DaoCategoria) Class.forName(daoCategoriaClase).getConstructor(String.class, String.class, String.class).newInstance(url, user, pass);
 		} catch (IOException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException | ClassNotFoundException e) {
 			throw new FabricaException("No se ha podido inicializar la fábrica", e);
 		}
@@ -27,6 +32,10 @@ public class Fabrica {
 	
 	public static DaoProducto getDaoProducto() {
 		return daoProducto;
+	}
+
+	public static DaoCategoria getDaoCategoria() {
+		return daoCategoria;
 	}
 	
 }
