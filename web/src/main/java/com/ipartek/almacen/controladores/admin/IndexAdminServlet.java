c728f514aaeab1a4d782fbd4cf371f1c6ffddc28
Javier Lete <javier@eskura21.com>
2024-12-04 09:50:27 +0100

    Listado de administración funcional

--- web/src/main/java/com/ipartek/almacen/controladores/admin/IndexAdminServlet.java
+++ web/src/main/java/com/ipartek/almacen/controladores/admin/IndexAdminServlet.java
@@ -1,11 +1,8 @@
 package com.ipartek.almacen.controladores.admin;
 
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.ArrayList;
 
-import com.ipartek.almacen.pojos.Producto;
+import com.ipartek.almacen.fabrica.Fabrica;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -23,11 +20,8 @@ public class IndexAdminServlet extends HttpServlet {
 		// Convertir los datos
 		// Empaquetarlos en objetos
 		// Ejecutar la lógica de negocio
-		var productos = new ArrayList<Producto>();
-
-		for (int i = 1; i <= 5; i++) {
-			productos.add(new Producto((long)i, "Producto " + i, new BigDecimal(10 * i), LocalDate.of(2000 + i, i, i * 2)));
-		}
+		
+		var productos = Fabrica.getUsuarioNegocio().verProductos();
 
 		// Empaquetar datos para la pantalla
 		
