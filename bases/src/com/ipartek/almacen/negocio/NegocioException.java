a89ea07e32f3a8c1f306b8c43cf8b5790dd4ebb5
Javier Lete <javier@eskura21.com>
2024-12-10 09:54:17 +0100

    Lombok para excepciones personalizadas

--- bases/src/com/ipartek/almacen/negocio/NegocioException.java
+++ bases/src/com/ipartek/almacen/negocio/NegocioException.java
@@ -1,27 +1,8 @@
 package com.ipartek.almacen.negocio;
 
+import lombok.experimental.StandardException;
+
+@StandardException
 public class NegocioException extends RuntimeException {
-
-	public NegocioException() {
-		super();
-	}
-
-	public NegocioException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
-	public NegocioException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	public NegocioException(String message) {
-		super(message);
-	}
-
-	public NegocioException(Throwable cause) {
-		super(cause);
-	}
-
 	private static final long serialVersionUID = 2363279553592511662L;
-
 }
