68abb35b9e290195e09c8dd0fec95b78ad5706eb
Javier Lete <javier@eskura21.com>
2024-12-18 11:03:30 +0100

    obtenerTodos los productos en acceso a datos

--- amazonia/accesodatos/src/main/java/com/ipartek/formacion/amazonia/accesodatos/jpa/ProductoJpaDao.java
+++ amazonia/accesodatos/src/main/java/com/ipartek/formacion/amazonia/accesodatos/jpa/ProductoJpaDao.java
@@ -1,11 +1,19 @@
 package com.ipartek.formacion.amazonia.accesodatos.jpa;
 
+import java.util.Collection;
+
 import com.ipartek.formacion.amazonia.accesodatos.JpaDao;
 import com.ipartek.formacion.amazonia.accesodatos.ProductoDao;
+import com.ipartek.formacion.amazonia.entidades.Producto;
 
 public class ProductoJpaDao extends JpaDao implements ProductoDao {
 
 	public ProductoJpaDao(String nousado1, String nousado2, String nousado3) {
 		super(nousado1, nousado2, nousado3);
+	}
+
+	@Override
+	public Collection<Producto> obtenerTodos() {
+		return enTransaccion(em -> em.createQuery("from Producto", Producto.class).getResultList());
 	}
 }
