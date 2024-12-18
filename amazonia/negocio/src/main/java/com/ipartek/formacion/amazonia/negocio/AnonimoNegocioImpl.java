2cfc9fc737f46db2d1f37f98bb8d4dcea51b7191
Javier Lete <javier@eskura21.com>
2024-12-18 11:03:38 +0100

    listar productos en negocio

--- amazonia/negocio/src/main/java/com/ipartek/formacion/amazonia/negocio/AnonimoNegocioImpl.java
+++ amazonia/negocio/src/main/java/com/ipartek/formacion/amazonia/negocio/AnonimoNegocioImpl.java
@@ -1,12 +1,22 @@
 package com.ipartek.formacion.amazonia.negocio;
 
+import java.util.Collection;
+
+import com.ipartek.formacion.amazonia.accesodatos.ProductoDao;
 import com.ipartek.formacion.amazonia.accesodatos.UsuarioDao;
+import com.ipartek.formacion.amazonia.entidades.Producto;
 import com.ipartek.formacion.amazonia.entidades.Usuario;
 import com.ipartek.formacion.amazonia.fabrica.Fabrica;
 
 public class AnonimoNegocioImpl implements AnonimoNegocio {
 	UsuarioDao usuarioDao = Fabrica.getUsuarioDao();
+	ProductoDao productoDao = Fabrica.getProductoDao();
 	
+	@Override
+	public Collection<Producto> listarProductos() {
+		return productoDao.obtenerTodos();
+	}
+
 	@Override
 	public Usuario iniciarSesion(Usuario usuario) {
 		var usuarioCompleto = usuarioDao.obtenerPorEmail(usuario.getEmail());
