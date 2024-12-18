e3d7018180f7e9df3a24885511513511ab581e8e
Javier Lete <javier@eskura21.com>
2024-12-18 11:03:57 +0100

    Front Controller en presentación

--- amazonia/presentacionweb/src/main/java/com/ipartek/formacion/amazonia/presentacionweb/controladores/Globales.java
+++ amazonia/presentacionweb/src/main/java/com/ipartek/formacion/amazonia/presentacionweb/controladores/Globales.java
@@ -1,5 +1,9 @@
 package com.ipartek.formacion.amazonia.presentacionweb.controladores;
 
+import com.ipartek.formacion.amazonia.negocio.AnonimoNegocio;
+import com.ipartek.formacion.amazonia.negocio.AnonimoNegocioImpl;
+
 class Globales {
 	static final String VISTAS = "/WEB-INF/vistas";
+	static final AnonimoNegocio anonimoNegocio = new AnonimoNegocioImpl();
 }
--- amazonia/presentacionweb/src/main/java/com/ipartek/formacion/amazonia/presentacionweb/controladores/IndexServlet.java
+++ amazonia/presentacionweb/src/main/java/com/ipartek/formacion/amazonia/presentacionweb/controladores/IndexServlet.java
@@ -7,11 +7,33 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/index")
+import static com.ipartek.formacion.amazonia.presentacionweb.controladores.Globales.*;
+
+@WebServlet("/fc/*")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher(Globales.VISTAS + "/index.jsp").forward(request, response);
+		this.request = request;
+		this.response = response;
+		
+		switch(request.getPathInfo()) {
+		case "/" -> index();
+		default -> response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+		}
+	}
+	
+	private void index() throws ServletException, IOException {
+		var productos = anonimoNegocio.listarProductos();
+		
+		request.setAttribute("productos", productos);
+		reenviar("/index.jsp");
+	}
+
+	private void reenviar(String vista) throws ServletException, IOException {
+		request.getRequestDispatcher(VISTAS + vista).forward(request, response);
 	}
 }
