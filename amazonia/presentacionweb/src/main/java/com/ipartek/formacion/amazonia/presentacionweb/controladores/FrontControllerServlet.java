7dfc659954834e881dc1bc5047247833fedc6afc
Javier Lete <javier@eskura21.com>
2024-12-19 09:07:27 +0100

    "/presentacionweb/fc" a "/presentacionweb"

---
+++ amazonia/presentacionweb/src/main/java/com/ipartek/formacion/amazonia/presentacionweb/controladores/FrontControllerServlet.java
@@ -0,0 +1,41 @@
+package com.ipartek.formacion.amazonia.presentacionweb.controladores;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.ipartek.formacion.amazonia.presentacionweb.controladores.Globales.*;
+
+@WebServlet("/fc/*")
+public class FrontControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.request = request;
+		this.response = response;
+		
+		String pathInfo = request.getPathInfo() == null ? "/" : request.getPathInfo();
+		
+		switch(pathInfo) {
+		case "/" -> index();
+		default -> response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+		}
+	}
+	
+	private void index() throws ServletException, IOException {
+		var productos = anonimoNegocio.listarProductos();
+		
+		request.setAttribute("productos", productos);
+		reenviar("/index.jsp");
+	}
+
+	private void reenviar(String vista) throws ServletException, IOException {
+		request.getRequestDispatcher(VISTAS + vista).forward(request, response);
+	}
+}
--- amazonia/presentacionweb/src/main/java/com/ipartek/formacion/amazonia/presentacionweb/controladores/IndexServlet.java
+++ amazonia/presentacionweb/src/main/java/com/ipartek/formacion/amazonia/presentacionweb/controladores/IndexServlet.java
@@ -1,39 +1,19 @@
 package com.ipartek.formacion.amazonia.presentacionweb.controladores;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-import static com.ipartek.formacion.amazonia.presentacionweb.controladores.Globales.*;
-
-@WebServlet("/fc/*")
+@WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
-	private HttpServletRequest request;
-	private HttpServletResponse response;
-	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		this.request = request;
-		this.response = response;
-		
-		switch(request.getPathInfo()) {
-		case "/" -> index();
-		default -> response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-		}
+		response.sendRedirect(request.getContextPath() + "/fc");;
 	}
 	
-	private void index() throws ServletException, IOException {
-		var productos = anonimoNegocio.listarProductos();
-		
-		request.setAttribute("productos", productos);
-		reenviar("/index.jsp");
-	}
-
-	private void reenviar(String vista) throws ServletException, IOException {
-		request.getRequestDispatcher(VISTAS + vista).forward(request, response);
-	}
 }
