2c652c435c7b9ce7ff255f9ec989c5d542213ea4
Javier Lete <javier@eskura21.com>
2024-12-30 11:30:21 +0100

    Uso de plantillas Thymeleaf con controladores

--- amazoniaspring/src/main/java/com/amazonia/controladores/IndexController.java
+++ amazoniaspring/src/main/java/com/amazonia/controladores/IndexController.java
@@ -2,8 +2,9 @@ package com.amazonia.controladores;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.amazonia.servicios.AnonimoService;
 
@@ -14,8 +15,20 @@ public class IndexController {
 	private AnonimoService anonimoService;
 	
 	@GetMapping("/")
-	@ResponseBody
-	public String index() {
-		return anonimoService.listarProductos().toString();
+	public String index(Model modelo) {
+		modelo.addAttribute("productos", anonimoService.listarProductos());
+		return "index";
+	}
+	
+	@GetMapping("/detalle")
+	public String detallePorId(Long id, Model modelo) {
+		modelo.addAttribute("producto", anonimoService.detalleProducto(id));
+		return "detalle";
+	}
+	
+	@GetMapping("/detalle/{url}")
+	public String detallePorUrl(@PathVariable String url, Model modelo) {
+		modelo.addAttribute("producto", anonimoService.detalleProducto(url));
+		return "detalle";
 	}
 }
