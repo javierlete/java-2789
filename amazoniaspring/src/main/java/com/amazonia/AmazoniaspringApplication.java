b4bdfe109a671615b909f6b4db1bc5d70d708fee
Javier Lete <javier@eskura21.com>
2024-12-30 10:12:46 +0100

    Prueba de repositorios

--- amazoniaspring/src/main/java/com/amazonia/AmazoniaspringApplication.java
+++ amazoniaspring/src/main/java/com/amazonia/AmazoniaspringApplication.java
@@ -1,13 +1,27 @@
 package com.amazonia;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.amazonia.repositorios.ProductoRepository;
+
 @SpringBootApplication
-public class AmazoniaspringApplication {
+public class AmazoniaspringApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmazoniaspringApplication.class, args);
+	}
+
+	@Autowired
+	private ProductoRepository repo;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// repo.save(Producto.builder().nombre("Prueba").precio(BigDecimal.ZERO).url("prueba").build());
+		
+		System.out.println(repo.findByUrl("prueba"));
 	}
 
 }
