e1d41012c8ee6af813c624c371193c3abd0f1975
Javier Lete <javier@eskura21.com>
2025-01-03 11:28:08 +0100

    Controlador de registro de usuarios

--- amazoniaspring/src/main/java/com/amazonia/controladores/IndexController.java
+++ amazoniaspring/src/main/java/com/amazonia/controladores/IndexController.java
@@ -3,9 +3,14 @@ package com.amazonia.controladores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.amazonia.entidades.Usuario;
+import com.amazonia.entidades.gruposvalidacion.UsuarioRegistro;
 import com.amazonia.servicios.AnonimoService;
 
 @Controller
@@ -23,6 +28,22 @@ public class IndexController {
 	@GetMapping("/login")
 	public String login() {
 		return "login";
+	}
+	
+	@GetMapping("/registro")
+	public String registro(Usuario usuario) {
+		return "registro";
+	}
+	
+	@PostMapping("/registro")
+	public String registroPost(@Validated(UsuarioRegistro.class) Usuario usuario, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "registro";
+		}
+		
+		anonimoService.registrarUsuario(usuario);
+		
+		return "redirect:/login";
 	}
 	
 	@GetMapping("/detalle")
