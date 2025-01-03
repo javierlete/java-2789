104dcc34dd3eaa636a581074dc4864bb871b537d
Javier Lete <javier@eskura21.com>
2025-01-03 10:39:54 +0100

    Uso en consola de BCrypt

--- amazoniaspring/src/main/java/com/amazonia/AmazoniaspringApplication.java
+++ amazoniaspring/src/main/java/com/amazonia/AmazoniaspringApplication.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.amazonia.repositorios.ProductoRepository;
-import com.amazonia.servicios.AnonimoService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class AmazoniaspringApplication implements CommandLineRunner {
@@ -15,20 +13,27 @@ public class AmazoniaspringApplication implements CommandLineRunner {
 		SpringApplication.run(AmazoniaspringApplication.class, args);
 	}
 
-	@Autowired
-	private ProductoRepository repo;
+//	@Autowired
+//	private ProductoRepository repo;
+	
+//	@Autowired
+//	private AnonimoService anonimoService;
 	
 	@Autowired
-	private AnonimoService anonimoService;
+	private PasswordEncoder passwordEncoder;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		// repo.save(Producto.builder().nombre("Prueba").precio(BigDecimal.ZERO).url("prueba").build());
 		// repo.save(Producto.builder().nombre("Prueba2").precio(new BigDecimal("1234.12")).url("prueba2").descripcion("Otra prueba").build());
 		
-		System.out.println(repo.findByUrl("prueba"));
+//		System.out.println(repo.findByUrl("prueba"));
 		
-		System.out.println(anonimoService.listarProductos());
+//		System.out.println(anonimoService.listarProductos());
+		
+		System.out.println(passwordEncoder.encode("javier"));
+		
+		System.out.println(passwordEncoder.matches("pepe", "$2a$12$p6RaIaXZEF64kA6g1pmkWeInYFw1ruDuNwpTO9j1rRXVH7Z5FQay2"));
 	}
 
 }
