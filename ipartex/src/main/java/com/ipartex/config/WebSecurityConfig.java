a14b4d1c48399a9682ce24867e475e3bdf51e0b8
Javier Lete <javier@eskura21.com>
2025-01-09 09:27:25 +0100

    Cambiamos a BCrypt para contraseñas

--- ipartex/src/main/java/com/ipartex/config/WebSecurityConfig.java
+++ ipartex/src/main/java/com/ipartex/config/WebSecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -36,8 +36,8 @@ public class WebSecurityConfig {
 
 	@Bean
 	PasswordEncoder passwordEncoder() {
-	    // return new BCryptPasswordEncoder();
-		return NoOpPasswordEncoder.getInstance();
+	    return new BCryptPasswordEncoder();
+		// return NoOpPasswordEncoder.getInstance();
 	}
 	
 	// AUTORIZACIÓN
