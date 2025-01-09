5a042fe337f6370155ba0fdfde652ae10cee7c88
Javier Lete <javier@eskura21.com>
2025-01-09 11:35:15 +0100

    Configuración de nombres de colecciones y path

--- ipartex/src/main/java/com/ipartex/repositorios/MensajeRepository.java
+++ ipartex/src/main/java/com/ipartex/repositorios/MensajeRepository.java
@@ -3,11 +3,13 @@ package com.ipartex.repositorios;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ipartex.entidades.Mensaje;
 
+@RepositoryRestResource(collectionResourceRel = "mensajes", path = "mensajes")
 public interface MensajeRepository extends CrudRepository<Mensaje, Long> {
-	Iterable<Mensaje> findMensajesByOrderByFechaDesc(); 
+	Iterable<Mensaje> findMensajesByOrderByFechaDesc();
 
 	@Query("select m.id from Mensaje m join m.lesGusta u where m.id=:id and u.email=:email")
 	Long comprobarMeGusta(@Param("id") Long id, @Param("email") String email);
--- ipartex/src/main/java/com/ipartex/repositorios/UsuarioRepository.java
+++ ipartex/src/main/java/com/ipartex/repositorios/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package com.ipartex.repositorios;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ipartex.entidades.Usuario;
 
+@RepositoryRestResource(collectionResourceRel = "usuarios", path = "usuarios")
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
 
 	Usuario findByEmail(String email);
